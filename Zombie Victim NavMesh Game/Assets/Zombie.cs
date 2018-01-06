66548470671d01ffa3daa831ecee189bd2b67e5c
ombharatiya <om.dkcarmel@gmail.com>
2018-01-06 06:48:18 +0530

    Added the game package and other assets including c# script

---
+++ Zombie Victim NavMesh Game/Assets/Zombie.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour {
+
+    public Transform victim;
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        // agent.destination = victim.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (victim)
+        {
+            agent.SetDestination(victim.position);
+        }
+	}
+}
